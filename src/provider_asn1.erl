476c7931d486d14e2aa77a1d6a1d8536c4f3445b
Kyle Nusbaum <KyleJNusbaum@gmail.com>
2015-06-20 13:09:20 -0500

    adding verbose option.

--- src/provider_asn1.erl
+++ src/provider_asn1.erl
@@ -16,8 +16,10 @@ init(State) ->
             {module, ?MODULE},            % The module implementation of the task
             {bare, true},                 % The task can be run by the user, always true
             {deps, ?DEPS},                % The list of dependencies
-            {example, "rebar3 asn"}, % How to use the plugin
-            {opts, []},                   % list of options understood by the plugin
+            {example, "rebar3 asn"},      % How to use the plugin
+            % list of options understood by the plugin
+            {opts, [{clean, $c, "clean", {boolean, false}, "Clean ASN.1 generated files"},
+                    {verbose, $v, "verbose", {boolean, false}, "Be Verbose."}]},
             {short_desc, "Compile ASN.1 with Rebar3"},
             {desc, "Compile ASN.1 with Rebar3"}
     ]),
@@ -26,52 +28,75 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    lists:foreach(fun process_app/1, rebar_state:project_apps(State)),
-%    io:format("Project apps: ~p~n", [rebar_state:project_apps(State)]),
+    {Args, _} = rebar_state:command_parsed_args(State),
+    case proplists:get_value(clean, Args) of
+        true ->
+            do_clean(State);
+        _ ->
+            lists:foreach(fun (App) -> process_app(State, App) end, rebar_state:project_apps(State))
+    end,
     {ok, State}.
 
 -spec format_error(any()) ->  iolist().
 format_error(Reason) ->
     io_lib:format("~p", [Reason]).
 
-process_app(App) ->
+process_app(State, App) ->
     AppPath = rebar_app_info:dir(App),
     ASNPath = filename:join(AppPath, "asn1"),
     IncludePath = filename:join(AppPath, "include"),
     SrcPath = filename:join(AppPath, "src"),
 
     Asns = find_asn_files(ASNPath),
-    io:format("    Asns: ~p~n", [Asns]), 
-    lists:foreach(fun(AsnFile) -> generate_asn(AppPath, AsnFile) end, Asns),
+    verbose_out(State, "    Asns: ~p", [Asns]), 
+    lists:foreach(fun(AsnFile) -> generate_asn(State, AppPath, AsnFile) end, Asns),
 
-    io:format("ERL files: ~p~n", [filelib:wildcard("*.erl", IncludePath)]),
+    verbose_out(State, "ERL files: ~p", [filelib:wildcard("*.erl", IncludePath)]),
     lists:foreach(fun(ErlFile) -> 
                           F = filename:join(IncludePath, ErlFile),
                           Dest = filename:join(SrcPath, ErlFile),
-                          io:format("Moving: ~p~n", [F]),
-                          io:format("~p~n", [file:rename(F, Dest)]) end, 
+                          verbose_out(State, "Moving: ~p", [F]),
+                          verbose_out(State, "~p", [file:rename(F, Dest)]) end, 
                   filelib:wildcard("*.erl", IncludePath)),
     
-    io:format("DB files: ~p~n", [filelib:wildcard("*.asn1db", IncludePath)]),
+    verbose_out(State, "DB files: ~p", [filelib:wildcard("*.asn1db", IncludePath)]),
     lists:foreach(fun(DBFile) -> 
                           F = filename:join(IncludePath, DBFile),
                           Dest = filename:join(SrcPath, DBFile),
-                          io:format("Moving: ~p~n", [F]),
-                          io:format("~p~n", [file:rename(F, Dest)]) end, 
+                          verbose_out(State, "Moving: ~p", [F]),
+                          verbose_out(State, "~p", [file:rename(F, Dest)]) end, 
                   filelib:wildcard("*.asn1db", IncludePath)),
 
-    io:format("BEAM files: ~p~n", [filelib:wildcard("*.beam", IncludePath)]),
+    verbose_out(State, "BEAM files: ~p", [filelib:wildcard("*.beam", IncludePath)]),
     lists:foreach(fun(BeamFile) -> 
                           F = filename:join(IncludePath, BeamFile),
-                          io:format("Moving: ~p~n", [F]),
-                          io:format("~p~n", [file:delete(F)]) end, 
+                          verbose_out(State, "Moving: ~p", [F]),
+                          verbose_out(State, "~p", [file:delete(F)]) end, 
                   filelib:wildcard("*.beam", IncludePath)),
     ok.
 
 find_asn_files(Path) ->
     [filename:join(Path, F) || F <- filelib:wildcard("*.asn1", Path)].
 
-generate_asn(Path, AsnFile) ->
-    io:format("Generating ASN.1 stuff.~n"),
-    asn1ct:compile(AsnFile, [ber, verbose, {outdir, filename:join(Path, "include")}]).
-    
+generate_asn(State, Path, AsnFile) ->
+    rebar_api:info("Generating ASN.1 files.", []),
+    {Args, _} = rebar_state:command_parsed_args(State),
+    CompileArgs = 
+        case proplists:get_value(verbose, Args) of
+            true -> [ber, verbose, {outdir, filename:join(Path, "include")}];
+            _ -> [ber, {outdir, filename:join(Path, "include")}]
+        end,
+    asn1ct:compile(AsnFile, CompileArgs).
+
+
+do_clean(_State) ->
+    ok.
+
+verbose_out(State, FormatString, Args)->
+    {CommArgs, _} = rebar_state:command_parsed_args(State),
+    case proplists:get_value(verbose, CommArgs) of
+        true ->
+            rebar_api:info(FormatString, Args);
+        _ ->
+            ok
+    end.
