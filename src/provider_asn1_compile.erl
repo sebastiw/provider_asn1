7d9f48e4f7f99d2d3031086ccac04abd599c3763
Sebastian Weddmark Olsson <visnae@gmail.com>
2022-11-20 17:28:03 +0100

    Compile opts to take precedence over provider_asn1 args

--- src/provider_asn1_compile.erl
+++ src/provider_asn1_compile.erl
@@ -109,8 +109,7 @@ generate_asn(State, Path, AsnFile, AppPath) ->
     Encoding = proplists:get_value(encoding, Args),
     Verbose = proplists:get_value(verbose, Args),
     CompileOpts = fix_paths(AppPath, proplists:get_value(compile_opts, Args)),
-    CompileArgs = [verbose || Verbose] ++ [noobj, Encoding, {outdir, Path}]
-        ++ CompileOpts,
+    CompileArgs = CompileOpts ++ [verbose || Verbose] ++ [noobj, Encoding, {outdir, Path}],
     provider_asn1_util:verbose_out(State, "Beginning compile with opts: ~p", [CompileArgs]),
     case asn1ct:compile(AsnFile, CompileArgs) of
         {error, E} ->
