8f926bb414d67b643a8af7951835ddaec8e1c96d
Sebastian Weddmark Olsson <visnae@gmail.com>
2022-11-20 20:41:39 +0100

    cleanup .set.asn* and .asn files

--- src/provider_asn1_compile.erl
+++ src/provider_asn1_compile.erl
@@ -74,7 +74,7 @@ process_app(State, AppPath) ->
 move_asns(State, GenPath, SrcPath, IncludePath, Asns) ->
     lists:foreach(
       fun(AsnFile) ->
-              Base = asn_basename(AsnFile),
+              Base = provider_asn1_util:asn_basename(AsnFile),
               provider_asn1_util:move_file(State, GenPath, Base ++ ".erl", SrcPath),
               provider_asn1_util:delete_file(State, GenPath, Base ++ ".asn1db"),
               provider_asn1_util:move_file(State, GenPath, Base ++ ".hrl", IncludePath)
@@ -119,9 +119,6 @@ find_asn_files(Path) ->
 
 is_latest(ASNFileName, ASNPath, GenPath) ->
     Source = filename:join(ASNPath, ASNFileName),
-    TargetFileName = asn_basename(ASNFileName) ++ ".erl",
+    TargetFileName = provider_asn1_util:asn_basename(ASNFileName) ++ ".erl",
     Target = filename:join(GenPath, TargetFileName),
     filelib:last_modified(Source) > filelib:last_modified(Target).
-
-asn_basename(ASNFileName) ->
-    filename:basename(filename:basename(ASNFileName, ".asn1"), ".asn").
